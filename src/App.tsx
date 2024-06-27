3fdf901ba58bac521cdbf77bd1ad02bbcc98edac
Jomar <jomarhan.ng@gmail.com>
2024-06-27 16:30:22 -0400

    reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,7 @@
-import { useState } from "react";
 import "./App.css";
-import Private from "./components/auth/Private";
-import Profile from "./components/auth/Profile";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
-  return (
-    <div className="App">
-      <Private
-        name="Cecilia"
-        isLoggedIn={isLoggedIn}
-        component={Profile}
-        setIsLoggedIn={setIsLoggedIn}
-      />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
